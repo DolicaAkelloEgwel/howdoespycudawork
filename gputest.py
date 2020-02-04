de21d4585aebaf561faa3dfc618fa36b2e3fbec9
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-04 16:05:41 +0000

    comparing 10000x10000 array addition

--- gputest.py
+++ gputest.py
@@ -1,3 +1,18 @@
 import cupy as cp
+import numpy as np
+import time
 
-x_gpu = cp.array([1, 2, 3, 4])
+def cool_timer(func):
+    start = time.time()
+    func()
+    end = time.time()
+    print(end - start)
+
+
+def add_arrays(matrix_library, size):
+    first_array = matrix_library.random.rand(size, size)
+    second_array = matrix_library.random.rand(size, size)
+    matrix_library.add(first_array, second_array)
+    
+cool_timer(lambda: add_arrays(cp, 10000))
+cool_timer(lambda: add_arrays(np, 10000))
