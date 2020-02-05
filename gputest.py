26d2c28ea92425ac56477e4d7c9218483fd58d37
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-05 14:00:57 +0000

    comments

--- gputest.py
+++ gputest.py
@@ -2,42 +2,72 @@ import cupy as cp
 import numpy as np
 import time
 from matplotlib import pyplot as plt
+import pycuda.gpuarray as gpuarray
 
+# Create a function for timing imaging-related operations
 def cool_timer(func, lib, size, num_arrs):
-    arrays = create_arrays(lib, num_arrs)
+    arrays = create_arrays(lib, size, num_arrs)
     start = time.time()
     func(lib, *arrays)
     end = time.time()
     return end - start
 
-def create_arrays(matrix_library, num_arrs):
-    return [matrix_library.random.rand(size, size) for _ in range(num_arrs)]
+# Create a given number of arrays
+def create_arrays(matrix_library, size_tuple, num_arrs):
+    return [matrix_library.random.rand(*size_tuple) for _ in range(num_arrs)]
 
+# Add two arrays 
 def add_arrays(matrix_library, first_array, second_array):
     matrix_library.add(first_array, second_array)
     
+# Do something akin to background correction with made-up data
 def background_correction_test(matrix_library, data, dark, flat):
     matrix_library.subtract(data, dark, out=data)
     matrix_library.subtract(flat, dark, out=flat)
     matrix_library.true_divide(data, flat, out=data)
-    
-array_sizes = [100, 1000, 5000, 10000, 20000]
 
-names = {cp: "cupy", np: "numpy"}
+# Create a list of functions used in the parallel comparison test
+funcs = [add_arrays, background_correction_test]
 
+# Create lists of array sizes and the total number of pixels/elements
+array_sizes = [(10,100), (100,100), (100,1000), (1000,1000), (1000,2000), (2000,2000)]
+total_pixels = [x[0] * x[1] for x in array_sizes]
+
+# Create a dictionary for storing the run results
 results = {cp: dict(), np: dict()}
 
+# Loop through the different libraries
 for lib in [cp, np]:
+
+    # Create empty lists for the results
     results[lib][add_arrays] = []
     results[lib][background_correction_test] = []
+    
+    # Loop through the different array sizes
     for size in array_sizes:
         total_add = 0
         total_bc = 0
+        
+        # Run the functions for the current array size 10 times
         for _ in range(10):
             total_add += cool_timer(add_arrays, lib, size, 2)
             total_bc += cool_timer(background_correction_test, lib, size, 3)
+            
+        # Compute the average speed for the 10 runs
         results[lib][add_arrays].append(total_add / 10)
         results[lib][background_correction_test].append(total_bc / 10)
-        
-print(results[np][add_arrays])
-print(results[cp][add_arrays])
+
+speed_up = dict()
+
+# Determine the speed up by diving numpy time by gpu time
+for func in funcs:
+    speed_up[func] = np.divide(results[np][func], results[cp][func])
+
+# Plot speed up
+plt.plot(speed_up[background_correction_test],label="Background Correction", marker=".")
+plt.plot(speed_up[add_arrays],label="Add Arrays", marker= ".")
+plt.xticks(range(len(total_pixels)), total_pixels)
+plt.legend()
+plt.xlabel("Number of Elements")
+plt.ylabel("Average Time Improvement (Numpy Time / Cupy Time)")
+plt.show()
