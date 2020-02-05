db6d479a60cfc41f1c65e241349e63156c25ae3d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-05 09:33:58 +0000

    average from 10 runs

--- gputest.py
+++ gputest.py
@@ -1,13 +1,14 @@
 import cupy as cp
 import numpy as np
 import time
+import matplotlib
 
 def cool_timer(func, lib, size, num_arrs):
     arrays = create_arrays(lib, num_arrs)
     start = time.time()
     func(lib, *arrays)
     end = time.time()
-    print(end - start)
+    return end - start
 
 def create_arrays(matrix_library, num_arrs):
     return [matrix_library.random.rand(size, size) for _ in range(num_arrs)]
@@ -22,8 +23,16 @@ def background_correction_test(matrix_library, data, dark, flat):
     
 array_sizes = [100, 1000, 5000, 10000, 20000]
 
+names = {cp: "cupy", np: "numpy"}
+
 for size in array_sizes:
     for lib in [cp, np]:
-        cool_timer(add_arrays, lib, size, 2)
-        cool_timer(background_correction_test, lib, size, 3)
+        total_add = 0
+        total_bc = 0
+        for _ in range(10):
+            total_add += cool_timer(add_arrays, lib, size, 2)
+            total_bc += cool_timer(background_correction_test, lib, size, 3)
+        print("Library:", names[lib], "/ Size:", size)
+        print(total_add / 10)
+        print(total_bc / 10)
         print("")
