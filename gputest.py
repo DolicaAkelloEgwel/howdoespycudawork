98a0eec9aa6c86368d1fffdb15fcf1348af88ba2
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-07 08:39:58 +0000

    experimenting with numba

--- gputest.py
+++ gputest.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import pycuda.gpuarray as gpuarray
 import pycuda.driver as cuda
 import pycuda.autoinit
+from numba import jit
 
 MINIMUM_PIXEL_VALUE = 1e-9
 MAXIMUM_PIXEL_VALUE = 1e9
@@ -71,9 +72,30 @@ class PyCudaImplementation(ImagingTester):
         arr1 + arr2
 
     def background_correction(self, data, dark, flat):
-        self.arrays[0] - self.arrays[1]
-        self.arrays[2] - self.arrays[1]
-        self.arrays[0] / self.arrays[2]
+        data - dark
+        flat - dark
+        data / flat
+
+
+class NumbaImplementation(ImagingTester):
+    def __init__(self, size):
+        super().__init__(size)
+
+    @staticmethod
+    @jit("void(float64[:,:],float64[:,:])")
+    def add_arrays(arr1, arr2):
+        for i in range(len(arr1)):
+            for j in range(len(arr1[0])):
+                arr1[i][j] += arr2[i][j]
+
+    @staticmethod
+    @jit("void(float64[:,:],float64[:,:],float64[:,:])")
+    def background_correction(data, dark, flat):
+        for i in range(len(data)):
+            for j in range(len(data[0])):
+                data[i][j] -= dark[i][j]
+                flat[i][j] -= dark[i][j]
+                data[i][j] /= flat[i][j]
 
 
 # Create a function for timing imaging-related operations
@@ -95,7 +117,12 @@ array_sizes = [
 total_pixels = [x[0] * x[1] for x in array_sizes]
 
 # Create a dictionary for storing the run results
-implementations = [PyCudaImplementation, NumpyImplementation, CupyImplementation]
+implementations = [
+    PyCudaImplementation,
+    NumpyImplementation,
+    CupyImplementation,
+    NumbaImplementation,
+]
 results = {impl: dict() for impl in implementations}
 function_names = ["Add Arrays", "Background Correction"]
 
@@ -123,7 +150,7 @@ for ExecutionClass in implementations:
         )
 
         assert size == imaging_obj.arrays[0].shape
-        # Compute the average speed for the 10 runs
+
         results[ExecutionClass]["Add Arrays"].append(total_add)
         results[ExecutionClass]["Background Correction"].append(total_bc)
 
@@ -131,6 +158,7 @@ library_labels = {
     CupyImplementation: "cupy",
     NumpyImplementation: "numpy",
     PyCudaImplementation: "pycuda",
+    NumbaImplementation: "numba",
 }
 
 ## Plot adding times
