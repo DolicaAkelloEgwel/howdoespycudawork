73195284aa58c4e6c2d87532284ee81f362d4f70
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-07 16:01:34 +0000

    honest timing

--- gputest.py
+++ gputest.py
@@ -1,6 +1,7 @@
 import cupy as cp
 import numpy as np
 import timeit
+import time
 from matplotlib import pyplot as plt
 import pycuda.gpuarray as gpuarray
 import pycuda.driver as cuda
@@ -72,9 +73,9 @@ class PyCudaImplementation(ImagingTester):
         arr1 + arr2
 
     def background_correction(self, data, dark, flat):
-        data - dark
-        flat - dark
-        data / flat
+        data -= dark
+        flat -= dark
+        data /= flat
 
 
 class NumbaImplementation(ImagingTester):
@@ -82,26 +83,34 @@ class NumbaImplementation(ImagingTester):
         super().__init__(size)
 
     @staticmethod
-    @jit("void(float64[:,:],float64[:,:])", nopython=True)
+    @jit("void(float64[:,:,:],float64[:,:,:])", nopython=True)
     def add_arrays(arr1, arr2):
         for i in range(len(arr1)):
             for j in range(len(arr1[0])):
-                arr1[i][j] += arr2[i][j]
+                for k in range(len(arr1[0][0])):
+                    arr1[i][j][k] += arr2[i][j][k]
 
     @staticmethod
-    @jit("void(float64[:,:],float64[:,:],float64[:,:])", nopython=True)
+    @jit("void(float64[:,:,:],float64[:,:,:],float64[:,:,:])", nopython=True)
     def background_correction(data, dark, flat):
         for i in range(len(data)):
             for j in range(len(data[0])):
-                data[i][j] -= dark[i][j]
-                flat[i][j] -= dark[i][j]
-                data[i][j] /= flat[i][j]
+                for k in range(len(data[0][0])):
+                    data[i][j][k] -= dark[i][j][k]
+                    flat[i][j][k] -= dark[i][j][k]
+                    data[i][j][k] /= flat[i][j][k]
 
 
 # Create a function for timing imaging-related operations
-def cool_timer(imaging_obj, func):
-    cp.cuda.runtime.deviceSynchronize()
-    return timeit.timeit(func, number=20)
+def cool_timer(func):
+    total_time = 0
+    total_runs = 20
+    for _ in range(total_runs):
+        time.time()
+        func()
+        cp.cuda.runtime.deviceSynchronize()
+        total_time += time.time()
+    return total_time / total_runs
 
 
 # Create lists of array sizes and the total number of pixels/elements
@@ -110,23 +119,25 @@ array_sizes = [
     (100, 100),
     (100, 1000),
     (1000, 1000),
-    (1000, 2000),
     (2000, 2000),
     (2500, 2500),
-    (3000, 3000),
-    (4000, 4000),
 ]
-total_pixels = [x[0] * x[1] for x in array_sizes]
+total_pixels = [x * y for x, y in array_sizes]
 
 # Create a dictionary for storing the run results
 implementations = [
-    PyCudaImplementation,
-    NumpyImplementation,
     CupyImplementation,
-    NumbaImplementation,
+    NumpyImplementation,
+    PyCudaImplementation,
+    # NumbaImplementation,
 ]
 results = {impl: dict() for impl in implementations}
 function_names = ["Add Arrays", "Background Correction"]
+
+mempool = cp.get_default_memory_pool()
+
+with cp.cuda.Device(0):
+    mempool.set_limit(size=20 * 1024 ** 3)  # Not sure what this is doing
 
 # Loop through the different libraries
 for ExecutionClass in implementations:
@@ -134,6 +145,8 @@ for ExecutionClass in implementations:
     # Create empty lists for the results
     results[ExecutionClass]["Add Arrays"] = []
     results[ExecutionClass]["Background Correction"] = []
+
+    imaging_obj = None
 
     # Loop through the different array sizes
     for size in array_sizes:
@@ -144,17 +157,22 @@ for ExecutionClass in implementations:
         imaging_obj = ExecutionClass(size)
 
         # Run the functions for the current array size 10 times
-        total_add = cool_timer(
-            imaging_obj, lambda: imaging_obj.add_arrays(*imaging_obj.arrays[:2])
-        )
+        total_add = cool_timer(lambda: imaging_obj.add_arrays(*imaging_obj.arrays[:2]))
         total_bc = cool_timer(
-            imaging_obj, lambda: imaging_obj.background_correction(*imaging_obj.arrays)
+            lambda: imaging_obj.background_correction(*imaging_obj.arrays)
         )
 
         assert size == imaging_obj.arrays[0].shape
 
         results[ExecutionClass]["Add Arrays"].append(total_add)
         results[ExecutionClass]["Background Correction"].append(total_bc)
+
+        print(
+            "Used bytes:", mempool.used_bytes(), "Total bytes:", mempool.total_bytes()
+        )
+        del imaging_obj
+        mempool.free_all_blocks()
+
 
 library_labels = {
     CupyImplementation: "cupy",
@@ -172,7 +190,7 @@ for impl in implementations:
 
 plt.ylabel("Time Taken")
 plt.xticks(range(len(total_pixels)), total_pixels)
-plt.yscale("log")
+# plt.yscale("log")
 plt.legend()
 
 ## Plot Background Correction Times
@@ -186,7 +204,7 @@ for impl in implementations:
 
 plt.ylabel("Time Taken")
 plt.xticks(range(len(total_pixels)), total_pixels)
-plt.yscale("log")
+# plt.yscale("log")
 plt.xlabel("Number of Pixels/Elements")
 
 ## Plot speed-up for cupy
