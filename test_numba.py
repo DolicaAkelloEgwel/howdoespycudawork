0a0bfcb87e43dc3b1bf4c28392b8f90840afc279
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-02-19 15:49:18 +0000

    attempt at pinned memory for numba

--- test_numba.py
+++ test_numba.py
@@ -1,5 +1,6 @@
 from numba import vectorize, cuda
 import time
+import numpy as np
 
 from imagingtester import (
     ImagingTester,
@@ -12,8 +13,9 @@ from imagingtester import (
     TEST_PARALLEL_NUMBA,
     partition_arrays,
     NO_PRINT,
+    num_partitions_needed,
+    memory_needed_for_arrays,
 )
-from imagingtester import num_partitions_needed as number_of_partitions_needed
 from write_and_read_results import (
     write_results_to_file,
     ADD_ARRAYS,
@@ -27,10 +29,10 @@ PARALLEL_VECTORISE_MODE = "parallel vectorise"
 CUDA_VECTORISE_MODE = "cuda vectorise"
 CUDA_JIT_MODE = "cuda jit"
 
-MODES = [PARALLEL_VECTORISE_MODE, CUDA_VECTORISE_MODE, CUDA_JIT_MODE]
-
-if not TEST_PARALLEL_NUMBA:
-    MODES = MODES[1:]
+if TEST_PARALLEL_NUMBA:
+    MODES = [PARALLEL_VECTORISE_MODE, CUDA_VECTORISE_MODE, CUDA_JIT_MODE]
+else:
+    MODES = [CUDA_VECTORISE_MODE, CUDA_JIT_MODE]
 
 
 def get_free_bytes():
@@ -38,11 +40,9 @@ def get_free_bytes():
     return meminfo[0]
 
 
-FREE_BYTES = get_free_bytes()
-
-num_partitions_needed = lambda cpu_arrays: number_of_partitions_needed(
-    cpu_arrays, FREE_BYTES
-)
+def get_used_bytes():
+    meminfo = cuda.current_context().get_memory_info()
+    return meminfo[1]
 
 
 @vectorize(["{0}({0},{0})".format(DTYPE)], target="cuda")
@@ -50,8 +50,8 @@ def cuda_vectorise_add_arrays(elem1, elem2):
     return elem1 + elem2
 
 
-@vectorize("{0}({0},{0},{0})".format(DTYPE), target="cuda")
-def cuda_vectorise_background_correction(data, dark, flat):
+@vectorize("{0}({0},{0},{0},int32,int32)".format(DTYPE), target="cuda")
+def cuda_vectorise_background_correction(data, dark, flat, clip_min, clip_max):
     data -= dark
     flat -= dark
     if flat > 0:
@@ -75,7 +75,9 @@ def cuda_jit_add_arrays(arr1, arr2):
 
 
 @cuda.jit
-def cuda_jit_background_correction(data, dark, flat):
+def cuda_jit_background_correction(
+    data, dark, flat, clip_min=MINIMUM_PIXEL_VALUE, clip_max=MAXIMUM_PIXEL_VALUE
+):
     i, j, k = cuda.grid(3)
 
     if i < data.shape[0] and j < data.shape[1] and k < data.shape[2]:
@@ -87,10 +89,10 @@ def cuda_jit_background_correction(data, dark, flat):
         else:
             data[i][j][k] /= MINIMUM_PIXEL_VALUE
 
-        if data[i][j][k] < MINIMUM_PIXEL_VALUE:
-            data[i][j][k] = MINIMUM_PIXEL_VALUE
-        elif data[i][j][k] > MAXIMUM_PIXEL_VALUE:
-            data[i][j][k] = MAXIMUM_PIXEL_VALUE
+        if data[i][j][k] < clip_min:
+            data[i][j][k] = clip_min
+        elif data[i][j][k] > clip_max:
+            data[i][j][k] = clip_max
 
 
 @vectorize(["{0}({0},{0})".format(DTYPE)], nopython=True, target="parallel")
@@ -114,138 +116,196 @@ def parallel_background_correction(data, dark, flat):
     return data
 
 
+def get_synchronized_time():
+    cuda.synchronize()
+    return time.time()
+
+
 def time_function(func):
-    cuda.synchronize()
-    start = time.time()
+    start = get_synchronized_time()
     func()
-    cuda.synchronize()
-    return time.time() - start
+    return get_synchronized_time() - start
 
 
 class NumbaImplementation(ImagingTester):
-    def __init__(self, size, mode, dtype):
+    def __init__(self, size, dtype):
         super().__init__(size, dtype)
-
-        if mode == PARALLEL_VECTORISE_MODE:
-            self.add_arrays = parallel_add_arrays
-            self.background_correction = parallel_background_correction
-        elif mode == CUDA_VECTORISE_MODE:
-            self.add_arrays = cuda_vectorise_add_arrays
-            self.background_correction = cuda_vectorise_background_correction
-        elif mode == CUDA_JIT_MODE:
-            self.add_arrays = cuda_jit_add_arrays
-            self.background_correction = cuda_jit_background_correction
-
         self.warm_up()
         self.lib_name = LIB_NAME
+        self.stream = cuda.stream()
 
     def warm_up(self):
         """
         Give the CUDA functions a chance to compile.
         """
         warm_up_arrays = create_arrays((1, 1, 1), self.dtype)
-        self.add_arrays(*warm_up_arrays[:2])
-        self.background_correction(*warm_up_arrays)
+        add_arrays(*warm_up_arrays[:2])
+        background_correction(*warm_up_arrays)
 
-    def timed_add_arrays(self, runs):
+    def clear_cuda_memory(self, split_arrays=[]):
 
-        total_time = 0
-        n_partitions_needed = num_partitions_needed(self.cpu_arrays[:2])
-
-        if n_partitions_needed == 1:
-            for _ in range(runs):
-                total_time += time_function(
-                    lambda: self.add_arrays(*self.cpu_arrays[:2])
-                )
-
-        else:
-
-            split_arrays = partition_arrays(self.cpu_arrays[:2], n_partitions_needed)
-
-            for i in range(n_partitions_needed):
-                cpu_array_segments = [split_array[i] for split_array in split_arrays]
-                for _ in range(runs):
-                    total_time += time_function(
-                        lambda: self.add_arrays(*cpu_array_segments)
-                    )
-
-            self.clear_cuda_memory(split_arrays)
-        self.clear_cuda_memory()
-        self.print_operation_times(total_time, ADD_ARRAYS, runs)
-        return total_time / runs
-
-    def timed_background_correction(self, runs):
-        total_time = 0
-
-        n_partitions_needed = num_partitions_needed(self.cpu_arrays)
-
-        if n_partitions_needed == 1:
-
-            # gpu_arrays = self._send_arrays_to_gpu(self.cpu_arrays)
-
-            for _ in range(runs):
-                total_time += time_function(
-                    lambda: self.background_correction(*self.cpu_arrays)
-                )
-
-        else:
-
-            split_arrays = partition_arrays(self.cpu_arrays, n_partitions_needed)
-
-            for i in range(n_partitions_needed):
-
-                cpu_array_segments = [split_array[i] for split_array in split_arrays]
-                # gpu_arrays = self._send_arrays_to_gpu(self.cpu_arrays)
-
-                for _ in range(runs):
-                    total_time += time_function(
-                        lambda: self.background_correction(*cpu_array_segments)
-                    )
-
-            self.clear_cuda_memory(split_arrays)
-
-        self.clear_cuda_memory()
-
-        self.print_operation_times(total_time, BACKGROUND_CORRECTION, runs)
-        return total_time / runs
-
-    def clear_cuda_memory(self, split_arrays=None):
+        self.stream.synchronize()
 
         if not NO_PRINT:
             print("Free bytes before clearing memory:", get_free_bytes())
-        if split_arrays is not None:
+
+        if split_arrays:
             for array in split_arrays:
                 del array
                 array = None
         cuda.current_context().deallocations.clear()
+
         if NO_PRINT:
             return
         print("Free bytes after clearing memory:", get_free_bytes())
 
+    def _send_arrays_to_gpu(self, cpu_arrays, result_array):
+
+        gpu_arrays = []
+        arrays_to_transfer = cpu_arrays + [result_array]
+
+        with cuda.pinned(*arrays_to_transfer):
+            for arr in arrays_to_transfer:
+                gpu_arrays.append(cuda.to_device(arr, self.stream))
+        return gpu_arrays[:-1], gpu_arrays[-1]
+
+    def timed_imaging_operation(self, runs, alg, alg_name, n_arrs_needed):
+
+        # Synchronize and free memory before making an assessment about available space
+        self.clear_cuda_memory()
+
+        n_partitions_needed = num_partitions_needed(
+            self.cpu_arrays[:n_arrs_needed], get_free_bytes()
+        )
+
+        transfer_time = 0
+        operation_time = 0
+
+        if n_partitions_needed == 1:
+
+            cpu_result_array = np.empty_like(self.cpu_arrays[0])
+
+            # Time transfer from CPU to GPU
+            start = get_synchronized_time()
+            gpu_arrays, gpu_result_array = self._send_arrays_to_gpu(
+                self.cpu_arrays[:n_arrs_needed], cpu_result_array
+            )
+            transfer_time += get_synchronized_time() - start
+
+            # Repeat the operation
+            for _ in range(runs):
+                operation_time += time_function(
+                    lambda: alg(*gpu_arrays[:n_arrs_needed])
+                )
+
+            # Time the transfer from GPU to CPU
+            transfer_time += time_function(
+                lambda: gpu_result_array.copy_to_host(cpu_result_array, self.stream)
+            )
+
+            # Free the GPU arrays
+            self.clear_cuda_memory(gpu_arrays + [gpu_result_array])
+
+        else:
+
+            # Split the arrays
+            split_arrays = partition_arrays(
+                self.cpu_arrays[:n_arrs_needed], n_partitions_needed
+            )
+
+            for i in range(n_partitions_needed):
+
+                # Retrieve the segments used for this iteration of the operation
+                split_cpu_arrays = [
+                    split_arrays[k][i] for k in range(len(split_arrays))
+                ]
+
+                cpu_result_array = np.empty_like(split_cpu_arrays[i])
+
+                try:
+
+                    # Time transferring the segments to the GPU
+                    start = get_synchronized_time()
+                    gpu_arrays, gpu_result_array = self._send_arrays_to_gpu(
+                        split_cpu_arrays, cpu_result_array
+                    )
+                    transfer_time += get_synchronized_time() - start
+
+                except cuda.cudadrv.driver.CudaAPIError:
+
+                    # This shouldn't happen provided partitioning is working correctly...
+                    print(
+                        "Failed to make %s GPU arrays of size %s."
+                        % (n_arrs_needed + 1, split_cpu_arrays[0].shape)
+                    )
+                    print(
+                        "Used bytes:",
+                        get_used_bytes(),
+                        "/ Total bytes:",
+                        get_free_bytes(),
+                        "/ Space needed:",
+                        memory_needed_for_arrays(split_cpu_arrays),
+                    )
+                    break
+
+                # Carry out the operation on the slices
+                for _ in range(runs):
+                    operation_time += time_function(
+                        lambda: alg(*gpu_arrays[:n_arrs_needed])
+                    )
+
+                transfer_time += time_function(gpu_result_array.get)
+
+                # Free GPU arrays and partition arrays
+                self.clear_cuda_memory(split_cpu_arrays + gpu_arrays + gpu_result_array)
+
+        if transfer_time > 0 and operation_time > 0:
+            self.print_operation_times(operation_time, alg_name, runs, transfer_time)
+
+        return transfer_time + operation_time / runs
+
 
 for mode in MODES:
 
-    add_arrays = []
-    background_correction = []
+    if mode == PARALLEL_VECTORISE_MODE:
+        add_arrays = parallel_add_arrays
+        background_correction = parallel_background_correction
+    elif mode == CUDA_VECTORISE_MODE:
+        add_arrays = cuda_vectorise_add_arrays
+        background_correction = lambda data, dark, flat: cuda_vectorise_background_correction(
+            data, dark, flat, MINIMUM_PIXEL_VALUE, MAXIMUM_PIXEL_VALUE
+        )
+    elif mode == CUDA_JIT_MODE:
+        add_arrays = cuda_jit_add_arrays
+        background_correction = lambda data, dark, flat: cuda_jit_background_correction(
+            data, dark, flat, MINIMUM_PIXEL_VALUE, MAXIMUM_PIXEL_VALUE
+        )
+
+    add_arrays_results = []
+    background_correction_results = []
 
     for size in ARRAY_SIZES[:SIZES_SUBSET]:
 
+        imaging_obj = NumbaImplementation(size, DTYPE)
+
         try:
-            imaging_obj = NumbaImplementation(size, mode, DTYPE)
+            avg_add = imaging_obj.timed_imaging_operation(
+                N_RUNS, add_arrays, "adding", 2
+            )
+            avg_bc = imaging_obj.timed_imaging_operation(
+                N_RUNS, background_correction, "background correction", 3
+            )
 
-            cuda.synchronize()
-            avg_add = imaging_obj.timed_add_arrays(N_RUNS)
-            cuda.synchronize()
-            avg_bc = imaging_obj.timed_background_correction(N_RUNS)
-
-            add_arrays.append(avg_add)
-            background_correction.append(avg_bc)
+            add_arrays_results.append(avg_add)
+            background_correction_results.append(avg_bc)
 
         except cuda.cudadrv.driver.CudaAPIError:
-            print("Can't operate on arrays with size", size)
+            print("Can't operate on arrays with size:", size)
+            print("Free bytes during CUDA error:", get_free_bytes())
+            imaging_obj.clear_cuda_memory()
             break
 
-    write_results_to_file([LIB_NAME, mode], ADD_ARRAYS, add_arrays)
+    write_results_to_file([LIB_NAME, mode], ADD_ARRAYS, add_arrays_results)
     write_results_to_file(
-        [LIB_NAME, mode], BACKGROUND_CORRECTION, background_correction
+        [LIB_NAME, mode], BACKGROUND_CORRECTION, background_correction_results
     )
