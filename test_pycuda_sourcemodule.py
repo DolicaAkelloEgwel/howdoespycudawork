8ab267f2c19c6dd06381c1ff649b3f0c7d52588d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-03-06 11:27:57 +0000

    convert ints to numpy int32

--- test_pycuda_sourcemodule.py
+++ test_pycuda_sourcemodule.py
@@ -1,57 +1,80 @@
-from pycuda import gpuarray
-import pycuda.driver as drv
 from pycuda.compiler import SourceModule
 import numpy as np
 
-from imagingtester import create_arrays, DTYPE, SIZES_SUBSET
-from numpy_scipy_imaging_filters import numpy_background_correction
-from pycuda_test_utils import PyCudaImplementation, C_DTYPE, LIB_NAME
+from imagingtester import DTYPE, create_arrays
+from pycuda_test_utils import PyCudaImplementation
 
-from write_and_read_results import (
-    ARRAY_SIZES,
-    write_results_to_file,
-    ADD_ARRAYS,
-    BACKGROUND_CORRECTION,
-)
+median_filter_module = SourceModule(
+    """
+__device__ float find_median(float* neighb_array, const int N)
+{
+    int i, j;
+    float key;
 
-BLOCKSIZE = 32
+    for (i = 1; i < N; i++)
+    {
+        key = neighb_array[i];
+        j = i - 1;
 
-
-def iDivUp(a, b):
-    return a // b + 1
-
-
-blockDim = (BLOCKSIZE, 1, 1)
-
-
-mode = "sourcemodule"
-
-kernel_code = """
-// the kernel definition
-  __global__ void kernel(float *data, float *odata) {
-  int index = blockIdx.x * blockDim.x + threadIdx.x ;
-  odata[index] = data[index]+1.0f;
-
-  }
-  """.format(
-    C_DTYPE
-)
-
-add_kernel = """
-        __global__ void Add({0} *arr1, {0} *arr2)
+        while (j >= 0 && neighb_array[j] > key)
         {
-          int i = blockIdx.x * blockDim.x + threadIdx.x ;
-          int j = blockIdx.y * blockDim.y + threadIdx.y ;
-          int k = blockIdx.z * blockDim.z + threadIdx.z ;
-          arr1[i] += arr2[i];
+            neighb_array[j + 1] = neighb_array[j];
+            j = j - 1;
         }
-        """.format(
-    C_DTYPE
+        neighb_array[j + 1] = key;
+    }
+    return neighb_array[N / 2];
+}
+__global__ void median_filter(float* data_array, const float* padded_array, const int N_IMAGES, const int X, const int Y, const int filter_height, const int filter_width)
+{
+    unsigned int id_img = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int id_x = blockIdx.y*blockDim.y + threadIdx.y;
+    unsigned int id_y = blockIdx.z*blockDim.z + threadIdx.z;
+    unsigned int n_counter = 0;
+    unsigned int img_size =  X * Y;
+    unsigned int padded_img_size =  (X + filter_height - 1) * (Y + filter_width - 1);
+    unsigned int padded_img_width =  X + filter_height - 1;
+
+    float neighb_array[20];
+
+    if ((id_img < N_IMAGES) && (id_x < X) && (id_y < Y))
+    {
+        for (int i = id_x; i < id_x + filter_height; i++)
+        {
+            for (int j = id_y; j < id_y + filter_width; j++)
+            {
+                neighb_array[n_counter] = padded_array[(id_img * padded_img_size) + (i * padded_img_width) + j];
+                n_counter += 1;
+            }
+        }
+
+        if (0)
+        {
+            // find_median(neighb_array, filter_height * filter_width);
+            for (int i = 0; i < filter_width * filter_height; i++)
+                printf("%f ", neighb_array[i]);
+        }
+
+        data_array[(id_img * img_size) + (id_x * X) + id_y] = find_median(neighb_array, filter_height * filter_width);
+    }
+}
+"""
 )
 
-mod = SourceModule(kernel_code)
+median_filter = median_filter_module.get_function("median_filter")
 
-AddArrays = mod.get_function("Add")
+
+def pycuda_median_filter(data, padded_data, filter_height, filter_width):
+    median_filter(
+        data,
+        padded_data,
+        np.int32(data.shape[0]),
+        np.int32(data.shape[1]),
+        np.int32(data.shape[2]),
+        np.int32(filter_height),
+        np.int32(filter_width),
+        block=(10, 10, 10),
+    )
 
 
 class PyCudaSourceModuleImplementation(PyCudaImplementation):
@@ -61,50 +84,26 @@ class PyCudaSourceModuleImplementation(PyCudaImplementation):
         self.warm_up()
 
     def warm_up(self):
-        warm_up_size = (1, 1, 1)
-        cpu_arrays = create_arrays(warm_up_size, DTYPE)
-        gpu_arrays = self._send_arrays_to_gpu(cpu_arrays, 3)
-        # BackgroundCorrection(
-        #     gpu_arrays[0],
-        #     gpu_arrays[1],
-        #     gpu_arrays[2],
-        #     MINIMUM_PIXEL_VALUE,
-        #     MAXIMUM_PIXEL_VALUE,
-        # )
-        AddArrays(gpu_arrays[0], gpu_arrays[1])
+        warm_up_size = (2, 2, 2)
+        cpu_data_array = create_arrays(warm_up_size, DTYPE)[0]
+
+        filter_height = 3
+        filter_width = 3
+        pad_height = filter_height // 2
+        pad_width = filter_width // 2
+
+        padded_cpu_array = np.pad(
+            cpu_data_array,
+            pad_width=((0, 0), (pad_height, pad_height), (pad_width, pad_width)),
+            mode="reflect",
+        )
+        gpu_data_array, gpu_padded_array = self._send_arrays_to_gpu(
+            [cpu_data_array, padded_cpu_array]
+        )
+        pycuda_median_filter(
+            gpu_data_array, gpu_padded_array, filter_height, filter_width
+        )
 
 
-practice_array = np.ones(shape=(5, 5, 5)).astype(DTYPE)
-practice_array = gpuarray.to_gpu(practice_array)
-# AddArrays(practice_array, practice_array)
-assert np.all(practice_array.get() == 2)
-
-np_arrays = [np.random.uniform(low=0.0, high=20, size=(5, 5, 5)) for _ in range(3)]
-np_data, np_dark, np_flat = np_arrays
-cuda_data, cuda_dark, cuda_flat = [gpuarray.to_gpu(np_arr) for np_arr in np_arrays]
-# elementwise_background_correction(cuda_data, cuda_flat, cuda_dark)
-numpy_background_correction(np_dark, np_data, np_flat)
-assert np.allclose(np_data, cuda_data.get())
-
-add_arrays_results = []
-background_correction_results = []
-
-for size in ARRAY_SIZES[:SIZES_SUBSET]:
-
-    imaging_obj = PyCudaSourceModuleImplementation(size, DTYPE)
-
-    # # add_arrays_results.append(
-    #     imaging_obj.timed_imaging_operation(N_RUNS, AddArraysKernel, "adding", 2, 2)
-    # # )
-    # background_correction_results.append(
-    #     imaging_obj.timed_imaging_operation(
-    #         # N_RUNS, elementwise_background_correction, "background correction", 3, 3
-    #     )
-    # )
-
-write_results_to_file([LIB_NAME, mode], ADD_ARRAYS, add_arrays_results)
-write_results_to_file(
-    [LIB_NAME, mode], BACKGROUND_CORRECTION, background_correction_results
-)
-
-drv.Context.pop()
+test = PyCudaSourceModuleImplementation((20, 20, 20), DTYPE)
+test.warm_up()
